5015b7425ff8a203d3b5568e6d8109acb1eb29af
Florian Schwehn <fschwehn@gmail.com>
2019-12-29 14:50:01 +0100

    added command type

--- Sources/RediStack/Commands/BasicCommands.swift
+++ Sources/RediStack/Commands/BasicCommands.swift
@@ -115,6 +115,16 @@ extension RedisClient {
             .convertFromRESPValue(to: Int.self)
             .map { return $0 == 1 }
     }
+    
+    /// Returns the `RedisType` of the value stored at key.
+    /// If a key does not exist `RedisType.none` will be returned.
+    /// - Parameter key:The key to get the type for
+    @inlinable
+    public func type(_ key: String) -> EventLoopFuture<RedisType> {
+        let args = [RESPValue(bulk: key)]
+        return send(command: "TYPE", with: args)
+            .convertFromRESPValue()
+    }
 }
 
 // MARK: Scan
